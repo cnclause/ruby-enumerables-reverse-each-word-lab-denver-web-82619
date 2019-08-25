0262891376384c848bacaf034deaee095ac21617
Catherine Clause <cnclause123@gmail.com>
2019-08-25 14:31:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,9 @@ end
  #new_array = array.split(" ")
   #reversed_array = new_array.each {|x| x.reverse!}
   #return reversed_array.join(" ")
-#end
+#end 
+
+def reverse_each_word(array) 
+  new_array = array.split(" ") 
+  reversed_array = new_array.collect {|x| x.reverse}.join(" ") 
+end
