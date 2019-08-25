74d79bbd616060a8a571f99aaf1c55910d887d75
Catherine Clause <cnclause123@gmail.com>
2019-08-25 14:26:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)  
   new_sentence = sentence.split(" ")
-reversed_sentence = new_sentence.each {|word| word.reverse!}
+reversed_sentence = new_sentence.each {|word| word.reverse}
 return reversed_sentence.join(" ")
 end  
 
